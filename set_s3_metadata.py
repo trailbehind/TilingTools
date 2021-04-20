fceae105f1450b43ed0cceca87b9b28ff1067964
Tony Cannistra <tony.cannistra@gmail.com>
2021-04-20 12:55:13 -0700

    support for system defined metadata

--- set_s3_metadata.py
+++ set_s3_metadata.py
@@ -10,22 +10,66 @@ import boto3
 from botocore.exceptions import BotoCoreError
 from time import sleep
 
+## AWS S3 has special metadata that's "system-defined"
+## https://docs.aws.amazon.com/AmazonS3/latest/userguide/UsingMetadata.html
+## The S3 console shows these with hyphens, but Boto3 requires no hyphens. 
+AWS_SYSTEM_METADATA_KEYS = [
+    "CacheControl",
+    "ContentDisposition",
+    "ContentEncoding",
+    "ContentLanguage",
+    "ContentType",
+]
+
+def _get_existing_system_metadata(existing_object):
+    """
+    Extract system-defined metadata from s3.head_object response
+    """
+    existing_meta = {}
+    for syskey in AWS_SYSTEM_METADATA_KEYS:
+        if syskey in existing_object:
+            existing_meta[syskey] = existing_object[syskey]
+    return existing_meta
+
+def _prep_metadata(metadata):
+    """
+    Separate user and system defined metadata keys from metadata dict
+    passed from user input
+    """
+    sys_meta = {}
+    user_meta = {_k.replace("-", "").strip().lower() : _v for _k, _v in metadata.items()}
+    for syskey in AWS_SYSTEM_METADATA_KEYS: 
+        if syskey.lower() in user_meta:
+            sys_meta[syskey] = user_meta[syskey.lower()]
+            del user_meta[syskey.lower()]
+
+    return (sys_meta,user_meta)
 
 def replace_metadata(bucket, key, new_metadata):
     s3 = boto3.client('s3')
     try:
+        ## Get existing system and user defined metadata
         existing = s3.head_object(
             Key=key, Bucket=bucket
         )
-        existing_metadata = existing['Metadata']
+        existing_system_metadata = _get_existing_system_metadata(existing)
+        existing_user_metadata = existing['Metadata']
         existing_etag = existing["ETag"]
-        existing_metadata.update(new_metadata)
+
+        ## get updates to user and system defined metadata
+        new_sys_meta, new_user_meta = _prep_metadata(new_metadata)
+        print(new_sys_meta, new_user_meta)
+
+        existing_user_metadata.update(new_user_meta)
+        existing_system_metadata.update(new_sys_meta)
+
         s3.copy_object(
             Key=key, Bucket=bucket, 
             CopySource={"Bucket": bucket, "Key": key}, 
             Metadata=new_metadata, 
             MetadataDirective='REPLACE', 
-            CopySourceIfMatch=existing_etag
+            CopySourceIfMatch=existing_etag, 
+            **existing_system_metadata
         )
         logging.debug(f"copy successful ({key})")
     except BotoCoreError as e:
@@ -40,7 +84,8 @@ def _main():
                       help="Turn on debug logging")
     parser.add_option("-q", "--quiet", action="store_true", dest="quiet",
                       help="turn off all logging")
-    parser.add_option("-s", "--set", action="append", dest="set", default=[])
+    parser.add_option("-s", "--set", action="append", dest="set", default=[],
+                      help="<KEY>=<VALUE>. AWS S3 System defined metadata keys should contain hyphens.")
     (options, args) = parser.parse_args()
  
     logging.basicConfig(level=logging.DEBUG if options.debug else
